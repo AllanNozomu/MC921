6c0f28484d7998bcd87193868d78bd1c55b965fa
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-09-26 08:42:05 -0300

    Update script to test things in separate

--- lab3/src/script.sh
+++ lab3/src/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+# set -x
 #compiling the Sum.g4 in java files
 java -jar "antlr-4.7.2-complete.jar" -no-listener -visitor Summer.g4
 #exporting the class path
@@ -7,19 +7,45 @@ export CLASSPATH=".:antlr-4.7.2-complete.jar:$CLASSPATH"
 #compiling the .java generated from Sum.g4 with MyParser.java and AddVisitor.java
 javac *.java
 
-# for i in $(seq 1 7); do
-  echo "Tokens"
-  java org.antlr.v4.gui.TestRig Summer root -tokens < ../tests/test${1}.sm
+echo "#################################################"
+echo "#                GERANDO TOKENS                 #"
+echo "#################################################"
 
-  echo "Tree"
-  java org.antlr.v4.gui.TestRig Summer root -tree < ../tests/test${1}.sm 
 
-  echo "GUI"
-  java org.antlr.v4.gui.TestRig Summer root -gui < ../tests/test${1}.sm 
+for i in $(seq 1 8); do
+    echo "TESTANDO ${i}========================================="
+    java org.antlr.v4.gui.TestRig Summer root -tokens < ../tests/test${i}.sm
+    echo "==================================================="
+done
 
-  echo "Verificando saida"
-  java MyParser < ../tests/test${1}.sm > temp_res.txt
-  diff ../tests/result${1}.txt temp_res.txt
-  echo "==================================================="
-# done
+echo "#################################################"
+echo "#                GERANDO TREES                  #"
+echo "#################################################"
+
+for i in $(seq 1 8); do
+    echo "TESTANDO ${i}========================================="
+    java org.antlr.v4.gui.TestRig Summer root -tree < ../tests/test${i}.sm 
+    echo "==================================================="
+done
+
+echo "#################################################"
+echo "#              GERANDO IMAGENS                  #"
+echo "#################################################"
+
+for i in $(seq 1 8); do
+    echo "TESTANDO ${i}========================================="
+    java org.antlr.v4.gui.TestRig Summer root -gui < ../tests/test${i}.sm > /dev/null &
+    echo "==================================================="
+done
+
+echo "#################################################"
+echo "#             VERIFICANDO SAIDAS                #"
+echo "#################################################"
+
+for i in $(seq 1 8); do
+    echo "TESTANDO ${i}========================================="
+    java MyParser < ../tests/test${i}.sm > temp_res.txt
+    diff ../tests/result${i}.txt temp_res.txt
+    echo "==================================================="
+done
 rm temp_res.txt
