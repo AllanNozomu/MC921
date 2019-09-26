086a0c1e4679515770f6b3bbbc0ed10e25cb5da1
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-09-26 08:44:25 -0300

    Complete Visitor with matching results txt files

--- lab3/src/AddVisitor.java
+++ lab3/src/AddVisitor.java
@@ -1,10 +1,14 @@
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class AddVisitor extends SummerBaseVisitor<Integer> {
     
-    HashMap<String, Object> variables = new HashMap<String, Object>(); 
+    HashSet<String> variables = new HashSet<String>(); 
+    HashSet<String> functions = new HashSet<String>(); 
+    HashMap<String, HashSet<String>> params = new HashMap<String, HashSet<String>>();
+    String currFunc = "";
 
     // Roots
     //---------------------------------------------------------
@@ -12,6 +16,7 @@ public class AddVisitor extends SummerBaseVisitor<Integer> {
     public Integer visitRootMultiLine(SummerParser.RootMultiLineContext ctx)
     {
         visit(ctx.line());
+        visit(ctx.root());
         return 0;
     }
 
@@ -26,45 +31,84 @@ public class AddVisitor extends SummerBaseVisitor<Integer> {
     @Override
     public Integer visitVarAssign(SummerParser.VarAssignContext ctx)
     {
-        variables.put(ctx.ID().getText(), visit(ctx.exprE()));
+        String varName = ctx.ID().getText();
+        if (variables.contains(varName))
+            System.out.println("Symbol already declared: " + varName);
+        else if (functions.contains(varName))
+            System.out.println("Symbol already declared: " + varName);
+        else
+            variables.add(ctx.ID().getText());
+
+        visit(ctx.exprE());
+        
         return 0;
     }
 
     @Override
     public Integer visitFuncDefinition(SummerParser.FuncDefinitionContext ctx)
     {
-        System.out.println("Function " + ctx.ID().getText());
-        ArrayList<Object> numbers = new ArrayList<>();
-        if (ctx.params() != null) {
-            for (SummerParser.ValueContext expr : ctx.params().value()) {
-                numbers.add(visit(expr));
-            }
+        String funcName = ctx.ID().getText();
+        currFunc = funcName;
+        
+        if (variables.contains(funcName))
+            System.out.println("Symbol already declared: " + funcName);
+        else if (functions.contains(funcName))
+            System.out.println("Symbol already declared: " + funcName);
+        else{
+            functions.add(currFunc);
         }
-        return null;
+
+        params.put(currFunc, new HashSet<String>());
+        visit(ctx.params());
+
+        visit(ctx.exprE());
+        currFunc = "";
+
+        return 0;
     }
 
     // Params
     //---------------------------------------------------------
-    // @Override
-    // public Integer visitParamsMulti(SummerParser.ParamsMultiContext ctx)
-    // {
-    //     ArrayList<Object> numbers = new ArrayList<>();
-    //     if (ctx.value() != null) {
-    //         for (TerminalNode tokenNode : ctx.value) {
-    //             numbers.add(Integer.valueOf(tokenNode.getText()));
-    //         }
-    //     }
-    //     System.out.println(numbers);
-    //     return 0;
-    // }
+     @Override
+     public Integer visitParamsMulti(SummerParser.ParamsMultiContext ctx)
+     {
+         String paramName = ctx.ID().getText();
+         params.get(currFunc).add(paramName);
+         return 1 + visit(ctx.params());
+     }
 
-    // @Override
-    // public Integer visitParamsSingle(SummerParser.ParamsSingleContext ctx)
-    // {
-    //     ArrayList<Object> params =  new ArrayList<Object>();
-    //     params.add(visit(ctx.value()));
-    //     return 0;
-    // }
+     @Override
+     public Integer visitParamsID(SummerParser.ParamsIDContext ctx)
+     {
+         String paramName = ctx.ID().getText();
+         params.get(currFunc).add(paramName);
+         return 1;
+     }
+     
+     @Override
+     public Integer visitParamsNone(SummerParser.ParamsNoneContext ctx)
+     {
+         return 0;
+     }
+
+    // Call
+     @Override
+     public Integer visitParamsCallMulti(SummerParser.ParamsCallMultiContext ctx)
+     {
+         return 1 + visit(ctx.paramsCall());
+     }
+
+     @Override
+     public Integer visitParamsCallID(SummerParser.ParamsCallIDContext ctx)
+     {
+         return 1;
+     }
+     
+     @Override
+     public Integer visitParamsCallNone(SummerParser.ParamsCallNoneContext ctx)
+     {
+         return 0;
+     }
 
     // Expressions
     //---------------------------------------------------------
@@ -81,6 +125,7 @@ public class AddVisitor extends SummerBaseVisitor<Integer> {
         return visit(ctx.exprE()) + visit(ctx.exprT());
     }
 
+    // T
     @Override
     public Integer visitExprTConversion(SummerParser.ExprTConversionContext ctx){
         return visit(ctx.exprF());
@@ -94,6 +139,7 @@ public class AddVisitor extends SummerBaseVisitor<Integer> {
         return visit(ctx.exprT()) + visit(ctx.exprF());
     }
 
+    // F
     @Override
     public Integer visitExprFParen(SummerParser.ExprFParenContext ctx){
         return visit(ctx.exprE());
@@ -102,25 +148,37 @@ public class AddVisitor extends SummerBaseVisitor<Integer> {
     public Integer visitExprFVal(SummerParser.ExprFValContext ctx){
         return visit(ctx.value());
     }
+
     @Override
     public Integer visitExprFFunc(SummerParser.ExprFFuncContext ctx){
-        ArrayList<Object> numbers = new ArrayList<>();
-        if (ctx.params() != null) {
-            for (SummerParser.ValueContext expr : ctx.params().value()) {
-                numbers.add(visit(expr));
-            }
-        }
-        return null; 
+        String funcName = ctx.ID().getText();
+
+        if (variables.contains(funcName))
+          System.out.println("Bad used symbol: " + funcName);
+        else if (!functions.contains(funcName))
+            System.out.println("Symbol undeclared: " + funcName);
+
+        Integer paramCount = visit(ctx.paramsCall());
+        if (params.get(funcName) != null && params.get(funcName).size() != paramCount)
+            System.out.println("Bad argument count: " + funcName);
+
+        return 0; 
     }
-
-
 
     // Values
     //---------------------------------------------------------
     @Override
     public Integer visitValueID(SummerParser.ValueIDContext ctx)
     {
-        ctx.ID().getText();
+        String varName = ctx.ID().getText(); 
+
+        if (currFunc == "" || !params.get(currFunc).contains(varName)){
+          if (functions.contains(varName))
+              System.out.println("Bad used symbol: " + varName);
+          else if (!variables.contains(varName))
+              System.out.println("Symbol undeclared: " + varName);
+        }
+ 
         return 0;
     }
 
