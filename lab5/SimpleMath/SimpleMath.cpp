848b80650f9766b266cf64fa35578f72aac8d467
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-10-31 08:31:42 -0300

    Simple math simple changes

--- lab5/SimpleMath/SimpleMath.cpp
+++ lab5/SimpleMath/SimpleMath.cpp
@@ -2,10 +2,13 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/raw_ostream.h"
+#include <unordered_map>
 #include <string>
 
 using namespace llvm;
-using namespace std;
+
+std::unordered_map<std::string, int> temp_to_index;
 
 namespace {
 struct SimpleMath : public FunctionPass {
@@ -16,10 +19,16 @@ struct SimpleMath : public FunctionPass {
     return false;
   }
 
-  void printOperand(Value *op) {
+  void printOperand(Value *op, std::string from) {
     if (isa<Instruction>(*op)) {
       Instruction *_I = cast<Instruction>(op);
-      errs() << *_I << "\n";
+
+        std::string str;
+        llvm::raw_string_ostream ss(str);
+        ss << *_I;
+
+      errs() << "I" << temp_to_index[str] << ":" << str << " | " << "I" << temp_to_index[from] << ":" << from << "\n";
+    // }
     } else if (isa<ConstantInt>(*op)) {
       ConstantInt *_V = cast<ConstantInt>(op);
       errs() << std::to_string(_V->getSExtValue()) << "\n";
@@ -28,7 +37,15 @@ struct SimpleMath : public FunctionPass {
 
   bool runOnFunction(Function &F) {
     for (BasicBlock &BB : F) {
+      temp_to_index = std::unordered_map<std::string, int>();
+
       for (Instruction &I : BB) {
+        std::string str;
+        llvm::raw_string_ostream ss(str);
+        ss << I;
+        errs() << str << "\n";
+        temp_to_index[str] = temp_to_index.size();
+
         Value *left;
         Value *right;
         switch (I.getOpcode()) {
@@ -38,11 +55,12 @@ struct SimpleMath : public FunctionPass {
           case Instruction::SDiv:
             left = I.getOperand(0);
             right = I.getOperand(1);
-            errs() << "Instruction: " << I << "\n";
-            errs() << "    Left operand: ";
-            printOperand(left);
-            errs() << "    Right operand: ";
-            printOperand(right);
+            // errs() << "Instruction: " << I << "\n";
+            // errs() << "    Left operand: ";
+            // printOperand(left);getOperand
+            // errs() << "    Right operand: ";
+            printOperand(left, str);
+            printOperand(right, str);
             break;
           default:
             errs() << "Instruction isn't arithmetic\n";
