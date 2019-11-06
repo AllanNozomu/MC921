a55f85053902d0d2ce696c292d5db9f27b3a3209
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-11-06 20:43:01 -0300

    Final first part

--- lab5/SimpleMath/SimpleMath.cpp
+++ lab5/SimpleMath/SimpleMath.cpp
@@ -1,14 +1,19 @@
 #include "llvm/Pass.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Constants.h"
+#include "llvm/IR/InstrTypes.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/raw_ostream.h"
 #include <unordered_map>
+#include <map>
 #include <string>
 
 using namespace llvm;
 
-std::unordered_map<std::string, int> temp_to_index;
+std::unordered_map<std::string, int> instruction_to_index;
+
+std::map<int, int> id_used_where;
 
 namespace {
 struct SimpleMath : public FunctionPass {
@@ -19,35 +24,39 @@ struct SimpleMath : public FunctionPass {
     return false;
   }
 
-  void printOperand(Value *op, std::string from) {
+  void printOperand(Value *op, std::string& from) {
     if (isa<Instruction>(*op)) {
       Instruction *_I = cast<Instruction>(op);
 
         std::string str;
         llvm::raw_string_ostream ss(str);
         ss << *_I;
-
-      errs() << "I" << temp_to_index[str] << ":" << str << " | " << "I" << temp_to_index[from] << ":" << from << "\n";
-    // }
-    } else if (isa<ConstantInt>(*op)) {
-      ConstantInt *_V = cast<ConstantInt>(op);
-      errs() << std::to_string(_V->getSExtValue()) << "\n";
-    }else errs() << op << "\n";
+        id_used_where[instruction_to_index[str]] = instruction_to_index[from];
+    }
+    // } else if (isa<ConstantInt>(*op)) {
+    //   ConstantInt *_V = cast<ConstantInt>(op);
+    //   errs() << std::to_string(_V->getSExtValue()) << "\n";
+    // }else errs() << op << "\n";
   }
 
   bool runOnFunction(Function &F) {
     for (BasicBlock &BB : F) {
-      temp_to_index = std::unordered_map<std::string, int>();
+      instruction_to_index = std::unordered_map<std::string, int>();
+      id_used_where = std::map<int, int>();
+
+      std::unordered_map<int, std::string> index_to_instruction;
 
       for (Instruction &I : BB) {
         std::string str;
         llvm::raw_string_ostream ss(str);
         ss << I;
-        errs() << str << "\n";
-        temp_to_index[str] = temp_to_index.size();
+        // errs() << I << "\n";
+        instruction_to_index[str] = instruction_to_index.size();
+        index_to_instruction[instruction_to_index[str]] = str;
 
         Value *left;
         Value *right;
+        Function *f;
         switch (I.getOpcode()) {
           case Instruction::Add:
           case Instruction::Sub:
@@ -55,17 +64,46 @@ struct SimpleMath : public FunctionPass {
           case Instruction::SDiv:
             left = I.getOperand(0);
             right = I.getOperand(1);
-            // errs() << "Instruction: " << I << "\n";
-            // errs() << "    Left operand: ";
-            // printOperand(left);getOperand
-            // errs() << "    Right operand: ";
             printOperand(left, str);
             printOperand(right, str);
             break;
-          default:
-            errs() << "Instruction isn't arithmetic\n";
+
+          case Instruction::Store:
+          // case Instruction::Load:
+            left = I.getOperand(0);
+            right = I.getOperand(1);
+            printOperand(left, str);
+            printOperand(right, str);
+            id_used_where[instruction_to_index[str]] = instruction_to_index[str];
+            
+            break;
+
+          case Instruction::Ret:
+            left = I.getOperand(0);
+            printOperand(left, str);
+            id_used_where[instruction_to_index[str]] = instruction_to_index[str];
+            break;
+
+          case Instruction::Call:
+            CallInst *callInst = cast<CallInst>(&I);
+            f = callInst->getCalledFunction();
+            left = I.getOperand(0);
+
+            for (int i = 0; i < std::distance(callInst->value_op_begin(), callInst->value_op_end()) - 1; ++i) {
+              left = I.getOperand(i);
+              printOperand(left, str);
+            }
+
+            if (f->getReturnType()->isVoidTy()){
+              id_used_where[instruction_to_index[str]] = instruction_to_index[str];
+            }
             break;
         }
+      }
+      for (auto &key_val : id_used_where) {
+        int left = key_val.first;
+        int right = key_val.second;
+        errs() << "I" << left << ": " << index_to_instruction[left] << " | " << "I" << right << ": " << index_to_instruction[right] << "\n";
       }
     }
     return false;
